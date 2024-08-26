551681a505e3965a80c1d4000ab1e8cb9e97c32c
druffko <christoph@rustige.me>
2024-08-26 16:10:54 +0200

    prepared settings & events

--- src/main/java/gg/druffko/youtubebot/config/Settings.java
+++ src/main/java/gg/druffko/youtubebot/config/Settings.java
@@ -1,4 +1,32 @@
 package gg.druffko.youtubebot.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Settings {
+
+    public static String discordToken = "";
+    public static String dcytChannel = "";
+    public static String postFile = "";
+    public static String ytToken = "";
+    public static String channelId = "";
+
+
+    public static void getConfig(){
+        String botConfigPath ="bot.properties";
+
+        Properties botProperties = new Properties();
+        try {
+            botProperties.load(new FileInputStream(botConfigPath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        discordToken = botProperties.getProperty("discordToken");
+        dcytChannel = botProperties.getProperty("rssChannel");
+        postFile = botProperties.getProperty("postFile");
+        ytToken = botProperties.getProperty("rssUrl");
+        channelId = botProperties.getProperty("channelId");
+    }
 }
--- src/main/java/gg/druffko/youtubebot/events/MessageEventListener.java
+++ src/main/java/gg/druffko/youtubebot/events/MessageEventListener.java
@@ -1,4 +1,14 @@
 package gg.druffko.youtubebot.events;
 
-public class MessageEventListener {
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class MessageEventListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event){
+        super.onMessageReceived(event);
+
+    }
 }
